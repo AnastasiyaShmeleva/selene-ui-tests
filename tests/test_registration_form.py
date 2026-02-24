b4fc92dce29bb1b38e9ec82061e4c1ad16927919
Anastasiya Shmeleva <shmeleva.nastya98@mail.ru>
2026-02-24 22:48:23 +0300

    Данные пользователя вынесены в отдельный датакласс

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,56 +1,42 @@
+from demoqa_tests.data import users
 from demoqa_tests.pages.registration_page import RegistrationPage
 from demoqa_tests import resource
 
 
 def test_registration_form():
     # Подготавливаем тестовые данные
-    form_data = {
-        'first_name': 'Ivan',
-        'last_name': 'Ivanov',
-        'email': 'test@test.ru',
-        'gender': 'Male',
-        'number': '9101112233',
-        'month': 'April',
-        'year': '1995',
-        'day': '17',
-        'subjects': ['History', 'English'],
-        'hobbies': ['Reading', 'Music'],
-        'photo': 'images.jpeg',
-        'current_address': '11 Lenin Avenue',
-        'state': 'Haryana',
-        'city': 'Karnal'
-    }
+    student = users.student
 
     registration_page = RegistrationPage()
     registration_page.open()
 
     registration_page \
-        .fill_first_name(form_data['first_name']) \
-        .fill_last_name(form_data['last_name']) \
-        .fill_email(form_data['email']) \
-        .select_gender(form_data['gender']) \
-        .fill_mobile_number(form_data['number']) \
+        .fill_first_name(student.first_name) \
+        .fill_last_name(student.last_name) \
+        .fill_email(student.email) \
+        .select_gender(student.gender) \
+        .fill_mobile_number(student.number) \
         .select_date_of_birth(
-            form_data['day'],
-            form_data['month'],
-            form_data['year'],
+            student.birthday.day,
+            student.birthday.month,
+            student.birthday.year,
         )
 
-    for subject in form_data['subjects']:
+    for subject in student.subjects:
         registration_page.select_subject(subject)
 
-    for hobby in form_data['hobbies']:
+    for hobby in student.hobbies:
         registration_page.select_hobby(hobby)
 
     # Пример через круглые скобки
     (
         registration_page
-        .upload_picture(resource.path(form_data["photo"]))
-        .fill_current_address(form_data['current_address'])
-        .fill_state(form_data['state'])
-        .fill_city(form_data['city'])
+        .upload_picture(resource.path(student.picture))
+        .fill_current_address(student.current_address)
+        .fill_state(student.state)
+        .fill_city(student.city)
         .submit()
     )
 
     # Проверка поп-ап
-    registration_page.should_have_registered(form_data)
+    registration_page.should_have_registered(student)
